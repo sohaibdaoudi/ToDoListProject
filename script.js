b563471ab97edcdc3ec0b3aeec20059269cdf4fb
SOHAIB DAOUDI <sohaib.daoudi.9404@gmail.com>
2024-12-14 15:26:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const signUpButton=document.getElementById('signUpButton');
-const signInButton=document.getElementById('signInButton');
-const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signup');
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const recoverLink = document.querySelector('#recoverLink a');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
+const recoverForm = document.getElementById('recover');
 
-signUpButton.addEventListener('click',function(){
-    signInForm.style.display="none";
-    signUpForm.style.display="block";
-})
-signInButton.addEventListener('click', function(){
-    signInForm.style.display="block";
-    signUpForm.style.display="none";
-})
+signUpButton.addEventListener('click', function () {
+    signInForm.style.display = "none";
+    recoverForm.style.display = "none";
+    signUpForm.style.display = "block";
+});
+
+signInButton.addEventListener('click', function () {
+    signUpForm.style.display = "none";
+    recoverForm.style.display = "none";
+    signInForm.style.display = "block";
+});
+
+recoverLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    signInForm.style.display = "none";
+    signUpForm.style.display = "none";
+    recoverForm.style.display = "block";
+});
